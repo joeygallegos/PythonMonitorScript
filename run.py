17912439aae9c6e84affe06472a175c541154c30
Joey Gallegos <joey@joeygallegos.com>
2023-09-21 23:21:37 -0500

    Update run.py
    Fixes #3

--- run.py
+++ run.py
@@ -326,7 +326,7 @@ if __name__ == "__main__":
                 get_email_markup(),
                 next_fail_ticks,
                 get_pretty_time(datetime.fromisoformat(get_incident_start_timestamp())),
-                get_incident_start_timestamp,
+                get_incident_start_timestamp(),
             )
 
     else:
